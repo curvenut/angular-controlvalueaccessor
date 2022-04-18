b6183f960996ae0e177eeb912805cf7c46e3c36a
Yves Leung-Tack <yves.leung-tack@montreal.ca>
2022-04-18 14:47:19 -0400

    feat: add hello1 and hello2 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Hello1Component } from './components/hello1/hello1.component';
+import { Hello2Component } from './components/hello2/hello2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Hello1Component,
+    Hello2Component
   ],
   imports: [
     BrowserModule
